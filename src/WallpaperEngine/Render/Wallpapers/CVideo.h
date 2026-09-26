f72e1a1e79f0d6ea1e727f4c9a1449ec77eb4b80
xoykor <138058470+xoykor@users.noreply.github.com>
2026-09-26 14:01:13 -0300

    refactor(renderer): propagate visual settings

--- src/WallpaperEngine/Render/Wallpapers/CVideo.h
+++ src/WallpaperEngine/Render/Wallpapers/CVideo.h
@@ -11,7 +11,8 @@ class CVideo final : public CWallpaper {
 public:
     CVideo (
 	const Wallpaper& wallpaper, RenderContext& context, AudioContext& audioContext,
-	const WallpaperState::TextureUVsScaling& scalingMode, const uint32_t& clampMode
+	const WallpaperState::TextureUVsScaling& scalingMode, const uint32_t& clampMode, const glm::vec2& uvOffset,
+	const PostProcessSettings& postProcess
     );
 
     ~CVideo () override;
